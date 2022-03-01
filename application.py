0f82081d5728b36128752e6d4e51a47fd4c28e89
Nishant-423 <64487568+Nishant-423@users.noreply.github.com>
2022-03-01 10:11:47 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,77 +4,57 @@ import requests
 import pyodbc
 
 #connecting to database
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=NISH-PC\SQLEXPRESS;'
-                      'Database=Practice;'
-                      'Trusted_Connection=No;'
-)
-                      #'UID=sa;'
-                      #'PWD=Bidya321@;')
+conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                      'Server=10.150.72.22;'
+                      'Database=ARMS;'
+                      'UID=dmdbobjects.prod;'
+                      'PWD=spice@123;'
+                      'Trusted_Connection=Yes;'
+                    )
 
 
-application=Flask(__name__)
+application = Flask(__name__)
 api = Api(application)
 
 # Getting the whole data of
 class Data(Resource):
     def get(self):
         cursor = conn.cursor()
-        data=cursor.execute("SELECT  CAST([EmpID] as Varchar) as ID,CAST([SALARY] as VARCHAR(max)) as SALARY,CAST(DeptID as varchar) as DeptID,CAST(EmpName as varchar) as NAME FROM [PRACTICE].[dbo].[Employee_Details]")
+        data=cursor.execute("select TRY_CAST(EmployeeID as Varchar) as EmployeeID,TRY_CAST(StaffID as Varchar) as StaffID,CAST(EmployeeName as Varchar) as EmpName,CAST(EmployeeCategory as Varchar) as EmployeeCategory,CAST(EmployeeStatus as Varchar) as EmployeeStatus,CAST(ResignedDate as Varchar) as ResignedDate,TRY_CAST(RepositoryShortName as Varchar) as Rank from com.Employee e left join com.REPOSITORY r on CAST(e.RankID as varchar)=cast(r.RepositoryID as varchar)")
         newlist=[]
         for i in data:
-            newdict={"ID":i[0],"Salary":i[1],"Date":i[2],"Name":i[3]}
+            newdict={"EmployeeID":i[0],"StaffID":i[1],"EmployeeName":i[2],"EmployeeCategory":i[3],"EmployeeStatus":i[4],"ResignedDate":i[5],"Rank":i[6]}
             newlist.append(newdict)
         return newlist
 
 
 #  Getting data by specifying Id,Name
-class Id_Name(Resource):
+class Id(Resource):
     def get(self):
         parser = reqparse.RequestParser()  # initialize
         parser.add_argument('Id', required=True)  # add args
-        parser.add_argument('Name', required=True)
+        #parser.add_argument('Name', required=True)
         #parser.add_argument('userId', required=True)  # add args
         args = parser.parse_args()
         ID=args['Id']
-        NAME=args['Name']
+        #NAME=args['Name']
         cursor = conn.cursor()
-        data=cursor.execute("SELECT  CAST([EmpID] as Varchar) as ID,CAST([SALARY] as VARCHAR(max)) as SALARY,CAST(DeptID as varchar) as DeptID,CAST(EmpName as varchar) as NAME FROM [PRACTICE].[dbo].[Employee_Details]",ID,NAME)
+        data=cursor.execute("select TRY_CAST(EmployeeID as Varchar) as EmployeeID,TRY_CAST(StaffID as Varchar) as StaffID,CAST(EmployeeName as Varchar) as EmpName,CAST(EmployeeCategory as Varchar) as EmployeeCategory,CAST(EmployeeStatus as Varchar) as EmployeeStatus,CAST(ResignedDate as Varchar) as ResignedDate,TRY_CAST(RepositoryShortName as Varchar) as Rank from com.Employee e left join com.REPOSITORY r on CAST(e.RankID as varchar)=cast(r.RepositoryID as varchar) where e.EmployeeId=?",ID)
         newlist=[]
         for i in data:
-            newdict={"ID":i[0],"Salary":i[1],"Date":i[2],"Name":i[3]}
+            newdict={"EmployeeID":i[0],"StaffID":i[1],"EmployeeName":i[2],"EmployeeCategory":i[3],"EmployeeStatus":i[4],"ResignedDate":i[5],"Rank":i[6]}
             newlist.append(newdict)
         return newlist
 
-
-# Getting data by specifying Id,Name,Salary         
-class Id_Name_Salary(Resource):
-    def get(self):
-        parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('Id')  # add args
-        parser.add_argument('Name')  # add args
-        parser.add_argument('Salary')  # add args
-        #parser.add_argument('userId', required=True)  # add args
-        args = parser.parse_args()
-        ID=args['Id']
-        NAME=args['Name']
-        SALARY=args['Salary']
-        cursor = conn.cursor()
-        data=cursor.execute("SELECT  CAST([EmpID] as Varchar) as ID,CAST([SALARY] as VARCHAR(max)) as SALARY,CAST(DeptID as varchar) as DeptID,CAST(EmpName as varchar) as NAME FROM [PRACTICE].[dbo].[Employee_Details]",ID,NAME,SALARY)
-        newlist=[]
-        for i in data:
-            newdict={"ID":i[0],"Salary":i[1],"Date":i[2],"Name":i[3]}
-            newlist.append(newdict)
-        return newlist
         
 
 #adding endpoints for API
 api.add_resource(Data, '/data')  # add endpoints
-api.add_resource(Id_Name, '/data/ID/Name')  # add endpoints
-api.add_resource(Id_Name_Salary, '/data/ID/Name/Salary')  # add endpoints
+api.add_resource(Id, '/data/Id')  # add endpoints
+
 
 
     
-#calling the main and running the whole code 
+#calling the main and runnning the whole code 
 if __name__ == '__main__':
     application.run()  # run our Flask app
